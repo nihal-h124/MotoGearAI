79881147e6e34bfaa3087995bb16600d1a4d49b8
nihal-h <f.socy10@gmail.com>
2025-11-14 23:37:28 +0100

    Created Supabase and integrated a waitlist page

---
+++ app/waitlist/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const waitlist = () => {
+  return (
+    <div>
+        <div>
+            <iframe style={{width: "100%"}} height="569" 
+                src="https://fa14fdab-c27f-48ad-b80e-4115fe2d6825.web.createdevserver.com/waitlist" 
+                title="Motorcycle Waitlist Page" frameBorder="0"></iframe>
+                </div>
+        </div>
+      
+  )
+}
+
+export default waitlist
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                // Apply these headers to all routes in your application.
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: "Contenty-Security-Policy",
+                        value: "frame-src 'self' https://fa14fdab-c27f-48ad-b80e-4115fe2d6825.created.app;",
+                    },
+                ],
+            },
+        ];
+    }
+};
 
 export default nextConfig;
